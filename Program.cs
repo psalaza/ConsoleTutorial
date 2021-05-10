912cc99ab3ecdbd5011e59d6221c377ffbbd962d
psalaza <43583947+psalaza@users.noreply.github.com>
2021-05-10 16:30:41 -0400

    Finished

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            y *=y;
+            y += 14;
+            y -=3;
+            Console.WriteLine(y);
+           // Console.WriteLine(y*y + 14 -3);
+
+        }
+    }
+}
